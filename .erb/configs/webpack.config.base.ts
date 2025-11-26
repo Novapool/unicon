0d8a11fbfdc9da28a6df5f52262b23f225b33758
Laith Assaf <laitho4325@gmail.com>
2025-11-26 15:27:11 -0500

    refactor: Update package.json name and author details, adjust dependency imports

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -4,7 +4,7 @@
 
 import webpack from 'webpack';
 import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
-import { dependencies as externals } from 'electron-react-boilerplate/package.json';
+import { dependencies as externals } from '../../release/app/package.json';
 import webpackPaths from './webpack.paths';
 
 const configuration: webpack.Configuration = {
